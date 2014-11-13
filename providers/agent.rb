295596a5290b1864b9810903eecf2fb0a310ebf8
Karol Drazek <karol.drazek@cognifide.com>
2014-11-13 12:21:09 +0100

    Initial commit

---
+++ providers/agent.rb
@@ -0,0 +1,52 @@
+#
+# Cookbook Name:: cq
+# Provider:: agent
+#
+# Copyright (C) 2014 Karol Drazek
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+ 
+def whyrun_supported?
+  true
+end
+
+# Create agent
+action :create do
+
+end
+
+# Delete agent
+action :delete do
+
+end
+
+# Enable agent
+action :enable do
+  
+end
+
+# Disable agent
+action :disable do
+  
+end
+
+# Activate agent
+action :activate do
+
+end
+
+# Deactivate agent
+action :deactivate do
+
+end
